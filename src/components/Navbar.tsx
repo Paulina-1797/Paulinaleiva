b5472741d15d160ff0264138f2d188058c4fbb1f
Paulina-1797 <126610488+Paulina-1797@users.noreply.github.com>
2026-05-15 18:07:07 -0400

    download

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -39,7 +39,7 @@ export default function Navbar() {
 
           <div>
             <button className="bg-primary hover:bg-primary/90 text-white px-6 py-2 rounded-full text-[10px] font-bold uppercase tracking-widest transition-all">
-              <a href="">CV.PDF</a>
+              <a href="CV/Paulina Leiva.pdf" Download>CV.PDF</a>
             </button>
           </div>
 
